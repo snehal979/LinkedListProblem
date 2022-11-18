0080e27c49091a300272dd3f2c74a95ce93a47f7
Snehal979 <bansodsnehal535@gmail.com>
2022-11-18 09:26:53 +0530

    inserted data in linked list at position

--- LinkedList.cs
+++ LinkedList.cs
@@ -10,6 +10,10 @@ namespace LinkedListProblem
     public class LinkedList
     {
         internal Node head;
+        /// <summary>
+        /// Uc1
+        /// </summary>
+        /// <param name="data"></param>
         public void Add(int data)
         {
             Node node = new Node(data);
@@ -44,6 +48,10 @@ namespace LinkedListProblem
                 }
             }
         }
+        /// <summary>
+        /// Uc2
+        /// </summary>
+        /// <param name="data"></param>
         public void AddInREverseOrder(int data)
         {
             Node newNode = new Node(data);
@@ -56,6 +64,40 @@ namespace LinkedListProblem
                 Node temp = this.head;
                 head = newNode;
                 head.next= temp;
+            }
+        }
+        /// <summary>
+        /// UC3
+        /// </summary>
+        /// <param name="position"></param>
+        /// <param name="data"></param>
+        public void InsertedAtParticularPosition(int position, int data)
+        {
+            Node newestNode = new Node(data);
+            if(this.head == null)
+            {
+                Console.WriteLine(newestNode);
+            }
+            if (position==0)
+            {
+                newestNode.next = this.head;
+                this.head = newestNode;
+                Console.WriteLine(this.head);
+            }
+            else
+            {
+                Node prev = null;
+                Node current = this.head;
+                int count = 0;
+                while(current != null && count < position)
+                {
+                    prev = current;
+                    current = current.next;
+                    count++;
+                }
+                newestNode.next  = prev.next;
+                prev.next =newestNode;
+                Console.WriteLine(this.head);
             }
         }
     }
--- Program.cs
+++ Program.cs
@@ -6,8 +6,10 @@
         {
             LinkedList linkedList = new LinkedList();
             linkedList.AddInREverseOrder(30);
-            linkedList.AddInREverseOrder(56);
+            //linkedList.AddInREverseOrder(56);
             linkedList.AddInREverseOrder(70);
+
+            linkedList.InsertedAtParticularPosition(1, 56);
             linkedList.Display();
         }
     }
