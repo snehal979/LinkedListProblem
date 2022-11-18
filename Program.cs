081f61ba42f7b861fe33a24864642805947a421b
Snehal979 <bansodsnehal535@gmail.com>
2022-11-18 08:48:36 +0530

    mainlinked list

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿namespace LinkedListProblem
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            LinkedList linkedList = new LinkedList();
+            linkedList.Add(30);
+            linkedList.Add(56);
+            linkedList.Add(70);
+            linkedList.Display();
+        }
+    }
+}
